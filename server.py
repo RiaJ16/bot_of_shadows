b8af46ba1de9066f39b617fed0ab9e4719c19440
Jair Nájera <jair.najera@htech.mx>
2021-02-27 20:45:26 -0600

    Excepción de no resultados
    Agregada excepción para que no se detenga el bot si la request http para ver los mensajes no se puede hacer.

--- server.py
+++ server.py
@@ -18,19 +18,22 @@ def make_reply(msg, nombre_, telegram_id_, tipo_):
 update_id = None
 while True:
     updates = bot_of_shadows.get_updates(offset=update_id)
-    updates = updates["result"]
-    if updates:
-        for item in updates:
-            update_id = item["update_id"]
-            try:
-                message = str(item["message"]["text"])
-            except (KeyError, IndexError):
-                message = None
-            if message:
-                tipo = item["message"]["chat"]["type"]
-                from_ = item["message"]["chat"]["id"]
-                nombre = item["message"]["from"]["first_name"]
-                telegram_id = item["message"]["from"]["id"]
-                reply = make_reply(message, nombre, telegram_id, tipo)
-                bot_of_shadows.send_message(reply, from_)
-    time.sleep(0.5)
+    try:
+        updates = updates["result"]
+        if updates:
+            for item in updates:
+                update_id = item["update_id"]
+                try:
+                    message = str(item["message"]["text"])
+                except (KeyError, IndexError):
+                    message = None
+                if message:
+                    tipo = item["message"]["chat"]["type"]
+                    from_ = item["message"]["chat"]["id"]
+                    nombre = item["message"]["from"]["first_name"]
+                    telegram_id = item["message"]["from"]["id"]
+                    reply = make_reply(message, nombre, telegram_id, tipo)
+                    bot_of_shadows.send_message(reply, from_)
+        time.sleep(0.5)
+    except KeyError:
+        pass
